9a75b6c34cdbb83d0452a2ec987d8b03d9e7c3db
shaunak <shaun.tul@gmail.com>
2020-01-07 12:18:07 -0800

    removed queuing, instead added 1s sleep to match perspective api QPS.

--- venv/src/testbot.py
+++ venv/src/testbot.py
@@ -32,25 +32,7 @@ cur = sql.cursor()
 cur.execute('CREATE TABLE IF NOT EXISTS oldposts(ID TEXT)')
 sql.commit()
 d = enchant.Dict("en_US")
-
-
-
-def enqueue_comment(comment_id):
-    queue = open("/home/shaunak/analyzer-bot/comment_queue.txt", "r+")
-    queued_comments = queue.readlines()
-    for str in queued_comments:
-        if comment_id == str:
-            queue.close()
-            return
-
-    queue.write(comment_id)
-
-def dequeue():
-    queue = open("/home/shaunak/analyzer-bot/comment_queue.txt", "r+")
-    queue.read(0)
-    
-
-
+queued_comments = []
 
 
 def is_word(word):
@@ -68,8 +50,6 @@ def is_a_real_user(username):
         return True
     except NotFound:
         return False
-
-
 
 
 def analyze_user_comments(username, limit=50):
@@ -110,19 +90,19 @@ def analyze_string_toxicity(comment):
 
 
 def analyze_user_toxicity(username, limit=50):
-    url = ('https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze' +
-           '?key=' + perspective_api_key)
-
     num_comments_analyzed = 0
     total_toxicity = 0.00
     try:
         redditor = reddit.redditor(username)
-        comments = redditor.comments.new(limit=limit)
+        comments = redditor.comments.new()
         for comment in comments:
+            time.sleep(1.2)  # 1 qps is free from perspective api
             num_comments_analyzed = num_comments_analyzed + 1
             comment_toxicity_rating = analyze_string_toxicity(comment.body)
             total_toxicity = total_toxicity + comment_toxicity_rating
         print(str(total_toxicity) + " overall toxicity over " + str(num_comments_analyzed) + " comments")
+        if num_comments_analyzed is 0:
+            return username + " has not posted any comments..."
         return "According to my analysis, \"" + username + "\" has an average toxicity rating of " \
                + str(total_toxicity / num_comments_analyzed) + " between all of their comments"
     except NotFound:
@@ -131,31 +111,39 @@ def analyze_user_toxicity(username, limit=50):
 
 def testbot():
     for comment in subreddit.stream.comments():
-        time.sleep(20)
+        # time.sleep(20)
         add_to_database = True
         cid = comment.id
         cur.execute('SELECT * FROM oldposts WHERE ID=?', [cid])
         print('checking if comment is already in database...')
         if not cur.fetchone():  # comment is not in database
-            print('comment not already in database, proceeding to do action on user...')
+            print('comment not already in database, checking for keyphrase')
             if USERNAME != comment.author.name and keyphrase in comment.body:
+                print("keyphrase present!")
                 word = comment.body.replace(keyphrase, '')  # removing the keyphrase from comment to pass to dictionary
                 target_user = re.search('\/u\/[A-Za-z0-9_-]{3,20}', word)
                 if target_user:
                     print(target_user.group(0))
                     target_username = target_user.group(0)[3:]
                     if bool(target_user) and is_a_real_user(target_username):
+                        post_limit_exceeded = False
+                        reply = analyze_user_toxicity(target_username)
                         try:
-                            reply = analyze_user_toxicity(target_username)
                             comment.reply(reply)
                             print('posted')
                         except APIException:
-                            add_to_database = False
-                            print('too frequent')
+                            post_limit_exceeded = True
+                            print('too frequent, sleeping for 20 seconds...')
+                            time.sleep(20)
+                        finally:
+                            if post_limit_exceeded:
+                                comment.reply(reply)
+                                print('posted')
+
+
+
         if add_to_database:
             cur.execute('INSERT INTO oldposts VALUES(?)', [cid])
             sql.commit()
 
-
-#testbot()
-enqueue_comment("lamo")
+testbot()
--- venv/test/bot_test.py
+++ venv/test/bot_test.py
@@ -1,29 +1,36 @@
 import unittest
+import praw
+import privatekeys
 
 import testbot as a
 
 real_user = "spez"
-fake_user = "jg98438gh9g023f39" # lets hope this user doesnt exist
+fake_user = "jg98438gh9g023f39"  # lets hope this user doesnt exist
 
-real_comment_id = "7cff0b" # pride and accomplishment
-comment_id_bottesting = "dwixky" # comment found on /r/bottesting
+real_comment_id = "7cff0b"  # pride and accomplishment
+comment_id_bottesting = "dwixky"  # comment found on /r/bottesting
+reddit = praw.Reddit(client_id=privatekeys.reddit_client_id,
+                     client_secret=privatekeys.reddit_client_secret_key,
+                     user_agent='console:testbot:1.0.0 (by /u/deltatwister)',
+                     username=privatekeys.username,
+                     password=privatekeys.password)
 
 
 
 class MyTestCase(unittest.TestCase):
+
+    def setUpClass(cls) -> None:
+        submission = reddit.get_submission(url=real_comment_id)
+
     def test_is_a_real_user(self):
         self.assertFalse(a.is_a_real_user(fake_user))
         self.assertTrue(a.is_a_real_user(real_user))
-
 
     def test_enqueue_comment(self):
         a.enqueue_comment(real_comment_id)
         a.enqueue_comment(comment_id_bottesting)
         self.assertEqual(a.dequeue(), real_comment_id)
         self.assertEqual(a.dequeue(), comment_id_bottesting)
-
-
-
 
 
 if __name__ == '__main__':
